b70b87b965d732c073f191dc991bca1f21a46206
Isaac Sukin <ice@icecreamyou.com>
2011-01-14 07:13:41 +0000

    #1004928 Drupal 7 port!

--- shorten.js
+++ shorten.js
@@ -1,16 +1,22 @@
 // $Id$
 
-Drupal.behaviors.shorten = function (context) {
-  shorten = $('#edit-shortened-url')[0];
-  if (shorten) {
-    shorten.select();
-    shorten.focus();
+(function ($) {
+
+Drupal.behaviors.shorten = {
+  attach: function (context) {
+    // Make sure we can run context.find().
+    var ctxt = $(context);
+    var shortenedURL = ctxt.find('.shorten-shortened-url');
+    var shorten = shortenedURL[0];
+    if (shorten) {
+      shorten.select();
+      shorten.focus();
+    }
+    shortenedURL.click(function() {
+      shorten.select();
+      shorten.focus();
+    });
   }
-  $('#edit-shortened-url').click(function() {
-    shorten.select();
-    shorten.focus();
-  });
-  $('#edit-this-shortened').click(function() {
-    $('#edit-this-shortened')[0].select().focus();
-  });
 }
+
+})(jQuery);
--- shorten_cs.js
+++ shorten_cs.js
@@ -1,18 +1,24 @@
 // $Id$
-Drupal.behaviors.shorten_cs = function (context) {
-  //Make sure we can run context.find().
-  if (!(context instanceof jQuery)) {
-    context = $(context);
-  }
-  if (context.find('.shorten-cs-apply-js input:radio[name=type]:checked').val() == 'text') {
-    context.find('#edit-tag-wrapper').hide();
-  }
-  context.find('.shorten-cs-apply-js input:radio[name=type]').change(function() {
-    if ($(this).val() == 'text') {
-      context.find('#edit-tag-wrapper').hide('fast');
+
+(function ($) {
+
+Drupal.behaviors.shorten_cs = {
+  attach: function (context) {
+    var tagItemName = '.form-item-tag';
+    // Make sure we can run context.find().
+    ctxt = $(context);
+    if (ctxt.find('.shorten-cs-apply-js input:radio[name=type]:checked').val() == 'text') {
+      ctxt.find('.shorten-cs-apply-js ' + tagItemName).hide();
     }
-    else {
-      context.find('#edit-tag-wrapper').show('fast');
-    }
-  });
+    ctxt.find('.shorten-cs-apply-js input:radio[name=type]').change(function() {
+      if ($(this).val() == 'text') {
+        ctxt.find('.shorten-cs-apply-js ' + tagItemName).hide('fast');
+      }
+      else {
+        ctxt.find('.shorten-cs-apply-js ' + tagItemName).show('fast');
+      }
+    });
+  }
 }
+
+})(jQuery);
